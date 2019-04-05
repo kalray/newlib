1fa3a12af6aa15051fc97cb95178846c45e99e7b
Julien Villette <jvillette@kalray.eu>
2019-04-05 10:48:16 +0200

    [K1, GENERIC] Declare _JBLEN and _JBTYPE.
    Test Plan: N/A
    
    Reviewers: O26 newlib!
    
    Differential Revision: https://phab.kalray.eu/D820
    
    Ref T9078

--- newlib/libc/include/machine/setjmp.h
+++ newlib/libc/include/machine/setjmp.h
@@ -1,6 +1,14 @@
 
 _BEGIN_STD_C
 
+#ifdef __k1__
+
+ /* callee saved + sp + ra + cs + r13, r14, loop regs */
+#define _JBLEN (14 + 1 + 1 + 1 + 1 + 1 + 3)
+#define _JBTYPE long long
+
+#endif /* __k1__ */
+
 #if defined(__or1k__) || defined(__or1knd__)
 #define _JBLEN 31 /* 32 GPRs - r0 */
 #define _JBTYPE unsigned long

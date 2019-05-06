ddaa8efd27aad29902e04c0b550793c664638fbe
Julien Villette <jvillette@kalray.eu>
2019-05-06 15:08:41 +0200

    [K1] libgloss k1-mbr
    Summary:
      Add copyrights and indentation
      Implementation of gettimeofday using power controller time stamps.
    
    Test Plan: N/A
    
    Reviewers: O26 newlib!
    
    Differential Revision: https://phab.kalray.eu/D822
    
    Ref T9078

---
+++ libgloss/k1-mbr/rmdir.c
@@ -0,0 +1,50 @@
+/*
+ * Copyright 2019
+ * Kalray Inc. All rights reserved.
+ *
+ * This software is furnished under license and may be used and copied only
+ * in accordance with the following terms and conditions.  Subject to these
+ * conditions, you may download, copy, install, use, modify and distribute
+ * modified or unmodified copies of this software in source and/or binary
+ * form. No title or ownership is transferred hereby.
+ *
+ * 1) Any source code used, modified or distributed must reproduce and
+ *    retain this copyright notice and list of conditions as they appear in
+ *    the source file.
+ *
+ * 2) No right is granted to use any trade name, trademark, or logo of
+ *    Kalray Inc.  The "Kalray Inc" name may not be
+ *    used to endorse or promote products derived from this software
+ *    without the prior written permission of Kalray Inc.
+ *
+ * 3) THIS SOFTWARE IS PROVIDED "AS-IS" AND ANY EXPRESS OR IMPLIED
+ *    WARRANTIES, INCLUDING BUT NOT LIMITED TO, ANY IMPLIED WARRANTIES OF
+ *    MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE, OR
+ *    NON-INFRINGEMENT ARE DISCLAIMED. IN NO EVENT SHALL KALRAY BE LIABLE
+ *    FOR ANY DAMAGES WHATSOEVER, AND IN PARTICULAR, KALRAY SHALL NOT BE
+ *    LIABLE FOR DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ *    CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ *    SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR
+ *    BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+ *    WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE
+ *    OR OTHERWISE), EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#include "errno.h"
+
+int sc_rmdir(const char *dir);
+
+int _rmdir(const char *dir)
+{
+  int ret;
+
+  ret = sc_rmdir(dir);
+
+  if( ret < 0 ) {
+    errno = ret * -1;
+    ret = -1;
+  }
+  return ret;
+}
+
+int rmdir(const char *dir) __attribute__ ((weak, alias ("_rmdir")));

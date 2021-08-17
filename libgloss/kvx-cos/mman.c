49cd0cadcf1ef688f8b6d9685d1f5da78f72d217
jhascoet <jhascoet@kalray.eu>
2021-08-17 15:55:36 +0200

    Expose mman.h functions and implement them for clusterOS
    Summary:
    We add mman.h functions and
    implement them for clusterOS for now.
    
    Test Plan: ClusterOS
    
    Reviewers: O26 newlib!
    
    Differential Revision: https://phab.kalray.eu/D14554

---
+++ libgloss/kvx-cos/mman.c
@@ -0,0 +1,213 @@
+/*
+ * Copyright 2021
+ * Kalray Inc. All rights reserved.
+ *
+ * This software is furnished under license and may be used and copied only
+ * in accordance with the following terms and conditions.  Subject to these
+ * conditions, you may download, copy, install, use, modify and distribute
+ * modified or unmodified copies of this software in source and/or binary
+ * form. No title or ownership is transferred hereby.
+ *
+ * 1) Any source code used, modified or distributed must reproduce and
+ *    retain this copyright notice and list of conditions as they appear in
+ *    the source file.
+ *
+ * 2) No right is granted to use any trade name, trademark, or logo of
+ *    Kalray Inc.  The "Kalray Inc" name may not be
+ *    used to endorse or promote products derived from this software
+ *    without the prior written permission of Kalray Inc.
+ *
+ * 3) THIS SOFTWARE IS PROVIDED "AS-IS" AND ANY EXPRESS OR IMPLIED
+ *    WARRANTIES, INCLUDING BUT NOT LIMITED TO, ANY IMPLIED WARRANTIES OF
+ *    MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE, OR
+ *    NON-INFRINGEMENT ARE DISCLAIMED. IN NO EVENT SHALL KALRAY BE LIABLE
+ *    FOR ANY DAMAGES WHATSOEVER, AND IN PARTICULAR, KALRAY SHALL NOT BE
+ *    LIABLE FOR DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ *    CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ *    SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR
+ *    BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+ *    WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE
+ *    OR OTHERWISE), EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#include <errno.h>
+#include <malloc.h>
+#include <hal/cos_syscall.h>
+#include <hal/cos_scall_no.h>
+#include <sys/mman.h>
+#include <sys/stat.h>
+#include <unistd.h>
+
+void *mmap(void *addr, size_t length,
+	int prot, int flags, int fd, off_t offset)
+{
+	void *ret = MAP_FAILED;
+	cos_syscall_type_t arg0, arg1, arg2, arg3, arg4, arg5;
+
+	if (!length || prot > (PROT_NONE | PROT_READ | PROT_WRITE | PROT_EXEC) ||
+		(flags != MAP_SHARED && flags != MAP_PRIVATE)) {
+		errno = -EINVAL;
+		goto exit_err;
+	}
+	arg0 = (cos_syscall_type_t)(uintptr_t)addr;
+	arg1 = (cos_syscall_type_t)length;
+	arg2 = (cos_syscall_type_t)prot;
+	arg3 = (cos_syscall_type_t)flags;
+	arg4 = (cos_syscall_type_t)fd;
+	arg5 = (cos_syscall_type_t)offset;
+	ret = (void*)(uintptr_t)__cos_syscall6(__NR_mppa_cos_mmap, arg0, arg1, arg2, arg3, arg4, arg5);
+	if (ret == MAP_FAILED) {
+		errno = -EINVAL;
+	}
+exit_err:
+	return ret;
+}
+
+int munmap(void *addr, size_t length)
+{
+	int ret;
+	cos_syscall_type_t arg0, arg1;
+
+	if (!addr || !length) {
+		errno = -EINVAL;
+		ret = -1;
+		goto exit_err;
+	}
+	arg0 = (cos_syscall_type_t)(uintptr_t)addr;
+	arg1 = (cos_syscall_type_t)length;
+	ret =__cos_syscall2(__NR_mppa_cos_munmap, arg0, arg1);
+	if (ret) {
+		errno = -EINVAL;
+	}
+exit_err:
+	return ret;
+}
+
+int mlockall(int flags)
+{
+	int ret = 0;
+
+	if (flags != MCL_CURRENT &&
+		flags != MCL_FUTURE &&
+		flags != MCL_ONFAULT) {
+		errno = -EINVAL;
+		ret = -1;
+	}
+
+	/*
+	 * This function does nothing but testing errors
+	 * since clusterOS maps and allocates statically
+	 * the physical memory
+	 */
+	return ret;
+}
+
+int munlockall(void)
+{
+	/*
+	 * This function does nothing since
+	 * clusterOS maps and allocates statically
+	 * the physical memory
+	 */
+	return 0;
+}
+
+int shm_open(const char *name, int oflag, mode_t mode)
+{
+	errno = -ENOSYS;
+	return -1;
+}
+
+int shm_unlink(const char *name)
+{
+	errno = -ENOSYS;
+	return -1;
+}
+
+int mlock(const void *addr, size_t len)
+{
+	int ret = 0;
+
+	if (!addr || !len) {
+		errno = -EINVAL;
+		ret = -1;
+	}
+
+	/*
+	 * This function does nothing since
+	 * clusterOS maps and allocates statically
+	 * the physical memory
+	 */
+	return ret;
+}
+
+int munlock(const void *addr, size_t len)
+{
+	int ret = 0;
+
+	if (!addr || !len) {
+		errno = -EINVAL;
+		ret = -1;
+	}
+
+	/*
+	 * This function does nothing since
+	 * clusterOS maps and allocates statically
+	 * the physical memory
+	 */
+	return ret;
+}
+
+int madvise(void *addr, size_t length, int advice)
+{
+	int ret = 0;
+
+	if (!addr || !length || advice < MADV_NORMAL || advice > MADV_PROTECT) {
+		errno = -EINVAL;
+		ret = -1;
+	}
+
+	/*
+	 * This function does nothing since
+	 * clusterOS maps and allocates statically
+	 * the physical memory
+	 */
+	return ret;
+}
+
+int posix_madvise(void *addr, size_t len, int advice)
+{
+	return madvise(addr, len, advice);
+}
+
+int mprotect(const void *addr, size_t len, int prot)
+{
+	int ret = -1;
+	cos_syscall_type_t arg0, arg1, arg2;
+
+	if (!addr || !len || prot > (PROT_NONE | PROT_READ | PROT_WRITE | PROT_EXEC)) {
+		errno = -EINVAL;
+		goto exit_err;
+	}
+	arg0 = (cos_syscall_type_t)(uintptr_t)addr;
+	arg1 = (cos_syscall_type_t)len;
+	arg2 = (cos_syscall_type_t)prot;
+	if (__cos_syscall3(__NR_mppa_cos_mprotect, arg0, arg1, arg2) != 0) {
+		errno = -EINVAL;
+		goto exit_err;
+	}
+	ret = 0;
+exit_err:
+	return ret;
+}
+
+int msync(void *addr, size_t length, int flags)
+{
+
+	if (!addr || !length || flags < MS_SYNC || flags > MS_INVALIDATE) {
+		errno = -EINVAL;
+		return -1;
+	}
+	/* No fs so nothing to do */
+	return 0;
+}
---
+++ newlib/libc/sys/cos/include/sys/mman.h
@@ -0,0 +1,115 @@
+/*
+ * Copyright 2021
+ * Kalray Inc. All rights reserved.
+ *
+ * This software is furnished under license and may be used and copied only
+ * in accordance with the following terms and conditions.  Subject to these
+ * conditions, you may download, copy, install, use, modify and distribute
+ * modified or unmodified copies of this software in source and/or binary
+ * form. No title or ownership is transferred hereby.
+ *
+ * 1) Any source code used, modified or distributed must reproduce and
+ *    retain this copyright notice and list of conditions as they appear in
+ *    the source file.
+ *
+ * 2) No right is granted to use any trade name, trademark, or logo of
+ *    Kalray Inc.  The "Kalray Inc" name may not be
+ *    used to endorse or promote products derived from this software
+ *    without the prior written permission of Kalray Inc.
+ *
+ * 3) THIS SOFTWARE IS PROVIDED "AS-IS" AND ANY EXPRESS OR IMPLIED
+ *    WARRANTIES, INCLUDING BUT NOT LIMITED TO, ANY IMPLIED WARRANTIES OF
+ *    MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE, OR
+ *    NON-INFRINGEMENT ARE DISCLAIMED. IN NO EVENT SHALL KALRAY BE LIABLE
+ *    FOR ANY DAMAGES WHATSOEVER, AND IN PARTICULAR, KALRAY SHALL NOT BE
+ *    LIABLE FOR DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ *    CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ *    SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR
+ *    BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+ *    WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE
+ *    OR OTHERWISE), EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#ifndef _SYS_MMAN_H_
+#define _SYS_MMAN_H_
+
+/*
+ * Protection attributes
+ */
+#define PROT_NONE 0x00 /* no permissions */
+#define PROT_READ 0x01 /* pages can be read */
+#define PROT_WRITE 0x02 /* pages can be written */
+#define PROT_EXEC 0x04 /* pages can be executed */
+
+/*
+ * Map flags attributes
+ */
+#define MAP_SHARED 0x0001 /* share changes */
+#define MAP_PRIVATE 0x0002 /* changes are private */
+#define MAP_FIXED 0x0010 /* map addr must be exactly as requested */
+#define MAP_ANONYMOUS 0x020 /* no banked by any file */
+#define MAP_ANON MAP_ANONYMOUS /* no banked by any file */
+#define MAP_HUGETLB 0x40000 /* huge page mapping */
+#define MAP_POPULATE 0x08000 /* preload pagetables */
+#define MAP_FAILED ((void *)-1) /* invalid address */
+
+/*
+ * Sync flags
+ */
+#define MS_SYNC 0x0000 /* msync synchronously */
+#define MS_ASYNC 0x0001 /* return immediately */
+#define MS_INVALIDATE 0x0002 /* invalidate all cached data */
+
+/*
+ * Madvise flags
+ */
+#define MADV_NORMAL 0 /* no further special treatment */
+#define MADV_RANDOM 1 /* expect random page references */
+#define MADV_SEQUENTIAL 2 /* expect sequential page references */
+#define MADV_WILLNEED 3 /* will need these pages */
+#define MADV_DONTNEED 4 /* dont need these pages */
+#define MADV_FREE 5 /* dont need these pages, and junk contents */
+#define MADV_NOSYNC 6 /* try to avoid flushes to physical media */
+#define MADV_AUTOSYNC 7 /* revert to default flushing strategy */
+#define MADV_NOCORE 8 /* do not include these pages in a core file */
+#define MADV_CORE 9 /* revert to including pages in a core file */
+#define MADV_PROTECT 10 /* protect process from pageout kill */
+
+/*
+ * POSIX madvice flags
+ */
+#define POSIX_MADV_NORMAL _MADV_NORMAL
+#define POSIX_MADV_RANDOM _MADV_RANDOM
+#define POSIX_MADV_SEQUENTIAL _MADV_SEQUENTIAL
+#define POSIX_MADV_WILLNEED _MADV_WILLNEED
+#define POSIX_MADV_DONTNEED _MADV_DONTNEED
+
+/*
+ * Mlock flags
+ */
+#define MCL_CURRENT 0x0001 /* Lock only current memory */
+#define MCL_FUTURE 0x0002 /* Lock all future memory as well */
+#define MCL_ONFAULT 0x0004 /* Map when they are faulted in */
+
+/*
+ * mman prototypes
+ */
+void *mmap(void *, size_t, int, int, int, off_t);
+int munmap(void *, size_t);
+
+int mlockall(int);
+int munlockall(void);
+
+int shm_open(const char *, int, mode_t);
+int shm_unlink(const char *);
+
+int mlock(const void *, size_t);
+int munlock(const void *, size_t);
+
+int madvise(void *, size_t, int);
+int posix_madvise(void *, size_t, int);
+
+int mprotect(const void *, size_t, int);
+int msync(void *, size_t, int);
+
+#endif /* !_SYS_MMAN_H_ */

d6a270704fddf259171828e245576cdc60866314
jhascoet <jhascoet@kalray.eu>
2021-11-26 18:02:02 +0100

    Add _POSIX_PATH_MAX to clusterOS features
    Summary:
    We add the _POSIX_PATH_MAX
    to the clusterOS features.
    
    Test Plan: CI
    
    Reviewers: O26 newlib!
    
    Differential Revision: https://phab.kalray.eu/D16800

--- newlib/libc/include/sys/features.h
+++ newlib/libc/include/sys/features.h
@@ -341,6 +341,7 @@ extern "C" {
 #define _POSIX_TIMERS                          1
 #define _POSIX_MONOTONIC_CLOCK                 200112L
 #define _POSIX_READER_WRITER_LOCKS             200112L
+#define _POSIX_PATH_MAX                        256
 #endif /* __CLUSTER_OS__ */
 
 /* RTEMS adheres to POSIX -- 1003.1b with some features from annexes.  */

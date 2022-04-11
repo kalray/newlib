40183a6812aaddac9e861abb67112969cfef7830
jhascoet <jhascoet@kalray.eu>
2022-04-11 17:44:05 +0200

    Add lstat syscall support
    Summary:
    We add the lstat syscall
    support to clusterOS backend.
    
    Test Plan: CI
    
    Reviewers: O26 newlib!
    
    Differential Revision: https://phab.kalray.eu/D19962

--- libgloss/kvx-cos/stat.c
+++ libgloss/kvx-cos/stat.c
@@ -8,35 +8,51 @@
 /***************************************************************
  *	stat
  ***************************************************************/
-int sc_stat(const char *filename, int *);
+int sc_stat(const char *filename, uint64_t *);
+int sc_lstat(const char *filename, uint64_t *);
+
+typedef int (*stat_func_t)(const char*, uint64_t*);
+
+static int
+internal_stat(const char *filename, struct stat *st, stat_func_t f)
+{
+	int ret;
+	uint64_t res[13];
+
+	ret = f(filename, res);
+
+	if( ret < 0 ) {
+		errno = ret * -1;
+		ret = -1;
+	} else {
+		st->st_dev = res[0];
+		st->st_ino = res[1];
+		st->st_mode = res[2];
+		st->st_nlink = res[3];
+		st->st_uid = res[4];
+		st->st_gid = res[5];
+		st->st_rdev = res[6];
+		st->st_size = res[7];
+		st->st_blksize = res[8];
+		st->st_blocks = res[9];
+		st->st_atime = res[10];
+		st->st_mtime = res[11];
+		st->st_ctime = res[12];
+	}
+
+	return ret;
+}
 
 int _stat(const char *filename, struct stat *st)
 {
-    int ret;
-    uint64_t res[13];
-
-    ret = sc_stat(filename, res);
-
-    if( ret < 0 ) {
-      errno = ret * -1;
-      ret = -1;
-    } else {
-	st->st_dev = res[0];
-	st->st_ino = res[1];
-	st->st_mode = res[2];
-	st->st_nlink = res[3];
-	st->st_uid = res[4];
-	st->st_gid = res[5];
-	st->st_rdev = res[6];
-	st->st_size = res[7];
-	st->st_blksize = res[8];
-	st->st_blocks = res[9];
-	st->st_atime = res[10];
-	st->st_mtime = res[11];
-	st->st_ctime = res[12];
-    }
-
-    return ret;
+	return internal_stat(filename, st, sc_stat);
 }
 
 int stat(const char *filename, struct stat *st) __attribute__ ((weak, alias ("_stat")));
+
+int _lstat(const char *filename, struct stat *st)
+{
+	return internal_stat(filename, st, sc_lstat);
+}
+
+int lstat(const char *filename, struct stat *st) __attribute__ ((weak, alias ("_lstat")));

1b382c469e082fb1f4ace776afdb5f00f6b4a86f
Paul Iannetta <piannetta@kalray.eu>
2023-02-21 15:10:29 +0100

    Fix asm syntax
    Previously our assembler considered all separators (",", "?", "=", "[]")
    to be the same, this is not the case anymore hence we need to fix all
    the misformed assembly.
    
    Test Plan: CI
    
    Reviewers: jhascoet, jvillette, O26 newlib
    
    Reviewed By: jhascoet, O26 newlib
    
    Maniphest Tasks: T20717
    
    Differential Revision: https://phab.kalray.eu/D26607

--- libgloss/kvx-elf/include/kvx/asm_context.h
+++ libgloss/kvx-elf/include/kvx/asm_context.h
@@ -78,84 +78,84 @@
 #define __KVX_CONTEXT64_SIZE  __KVX_CONTEXT_LAST
 
 .macro __kvx_context64_save to
-	sq    __KVX_CONTEXT_R0R1[\to], $r0r1        ## $r0 - $r1
+	sq    __KVX_CONTEXT_R0R1[\to]= $r0r1        ## $r0 - $r1
 	;;
 	get   $r2=$spc
-	sq    __KVX_CONTEXT_R2R3[\to], $r2r3        ## $r2 - $r3
+	sq    __KVX_CONTEXT_R2R3[\to]= $r2r3        ## $r2 - $r3
 	;;
 	get   $r3 = $ls
-	sq    __KVX_CONTEXT_R4R5[\to], $r4r5       ## $r4 - $r5
+	sq    __KVX_CONTEXT_R4R5[\to]= $r4r5       ## $r4 - $r5
 	;;
 	get   $r4 = $le
-	sq    __KVX_CONTEXT_R6R7[\to], $r6r7       ## $r6 - $r7
+	sq    __KVX_CONTEXT_R6R7[\to]= $r6r7       ## $r6 - $r7
 	;;
 	get   $r5 = $ra
-	sq    __KVX_CONTEXT_R8R9[\to], $r8r9       ## $r8 - $r9
+	sq    __KVX_CONTEXT_R8R9[\to]= $r8r9       ## $r8 - $r9
 	;;
 	get   $r6 = $sps
-	sq    __KVX_CONTEXT_R10R11[\to], $r10r11      ## $r10 - $r11
+	sq    __KVX_CONTEXT_R10R11[\to]= $r10r11      ## $r10 - $r11
 	;;
 	get   $r7 = $cs
-	sd    __KVX_CONTEXT_R13[\to], $r13            ## $r13
+	sd    __KVX_CONTEXT_R13[\to]= $r13            ## $r13
 	;;
 	get   $r8 = $lc
-	sq    __KVX_CONTEXT_R14R15[\to], $r14r15      ## $r14 - $r15
+	sq    __KVX_CONTEXT_R14R15[\to]= $r14r15      ## $r14 - $r15
 	;;
-	sq    __KVX_CONTEXT_R16R17[\to], $r16r17      ## $r16 - $r17
+	sq    __KVX_CONTEXT_R16R17[\to]= $r16r17      ## $r16 - $r17
 	;;
-	sq    __KVX_CONTEXT_R18R19[\to], $r18r19      ## $r18 - $r19
+	sq    __KVX_CONTEXT_R18R19[\to]= $r18r19      ## $r18 - $r19
 	;;
-	sq    __KVX_CONTEXT_R20R21[\to], $r20r21      ## $r20 - $r21
+	sq    __KVX_CONTEXT_R20R21[\to]= $r20r21      ## $r20 - $r21
 	;;
-	sq    __KVX_CONTEXT_R22R23[\to], $r22r23      ## $r22 - $r23
+	sq    __KVX_CONTEXT_R22R23[\to]= $r22r23      ## $r22 - $r23
 	;;
-	sq    __KVX_CONTEXT_R24R25[\to], $r24r25      ## $r24 - $r25
+	sq    __KVX_CONTEXT_R24R25[\to]= $r24r25      ## $r24 - $r25
 	;;
-	sq    __KVX_CONTEXT_R26R27[\to], $r26r27      ## $r26 - $r27
+	sq    __KVX_CONTEXT_R26R27[\to]= $r26r27      ## $r26 - $r27
 	;;
-	sq    __KVX_CONTEXT_R28R29[\to], $r28r29      ## $r28 - $r29
+	sq    __KVX_CONTEXT_R28R29[\to]= $r28r29      ## $r28 - $r29
 	;;
-	sq    __KVX_CONTEXT_R30R31[\to], $r30r31      ## $r30 - $r31
+	sq    __KVX_CONTEXT_R30R31[\to]= $r30r31      ## $r30 - $r31
 	;;
-	sq    __KVX_CONTEXT_R32R33[\to], $r32r33      ## $r32 - $r33
+	sq    __KVX_CONTEXT_R32R33[\to]= $r32r33      ## $r32 - $r33
 	;;
-	sq    __KVX_CONTEXT_R34R35[\to], $r34r35      ## $r34 - $r35
+	sq    __KVX_CONTEXT_R34R35[\to]= $r34r35      ## $r34 - $r35
 	;;
-	sq    __KVX_CONTEXT_R36R37[\to], $r36r37      ## $r36 - $r37
+	sq    __KVX_CONTEXT_R36R37[\to]= $r36r37      ## $r36 - $r37
 	;;
-	sq    __KVX_CONTEXT_R38R39[\to], $r38r39      ## $r38 - $r39
+	sq    __KVX_CONTEXT_R38R39[\to]= $r38r39      ## $r38 - $r39
 	;;
-	sq    __KVX_CONTEXT_R40R41[\to], $r40r41      ## $r40 - $r41
+	sq    __KVX_CONTEXT_R40R41[\to]= $r40r41      ## $r40 - $r41
 	;;
-	sq    __KVX_CONTEXT_R42R43[\to], $r42r43      ## $r42 - $r43
+	sq    __KVX_CONTEXT_R42R43[\to]= $r42r43      ## $r42 - $r43
 	;;
-	sq    __KVX_CONTEXT_R44R45[\to], $r44r45      ## $r44 - $r45
+	sq    __KVX_CONTEXT_R44R45[\to]= $r44r45      ## $r44 - $r45
 	;;
-	sq    __KVX_CONTEXT_R46R47[\to], $r46r47      ## $r46 - $r47
+	sq    __KVX_CONTEXT_R46R47[\to]= $r46r47      ## $r46 - $r47
 	;;
-	sq    __KVX_CONTEXT_R48R49[\to], $r48r49      ## $r48 - $r49
+	sq    __KVX_CONTEXT_R48R49[\to]= $r48r49      ## $r48 - $r49
 	;;
-	sq    __KVX_CONTEXT_R50R51[\to], $r50r51      ## $r50 - $r51
+	sq    __KVX_CONTEXT_R50R51[\to]= $r50r51      ## $r50 - $r51
 	;;
-	sq    __KVX_CONTEXT_R52R53[\to], $r52r53      ## $r52 - $r53
+	sq    __KVX_CONTEXT_R52R53[\to]= $r52r53      ## $r52 - $r53
 	;;
-	sq    __KVX_CONTEXT_R54R55[\to], $r54r55      ## $r54 - $r55
+	sq    __KVX_CONTEXT_R54R55[\to]= $r54r55      ## $r54 - $r55
 	;;
-	sq    __KVX_CONTEXT_R56R57[\to], $r56r57      ## $r56 - $r57
+	sq    __KVX_CONTEXT_R56R57[\to]= $r56r57      ## $r56 - $r57
 	;;
-	sq    __KVX_CONTEXT_R58R59[\to], $r58r59      ## $r58 - $r59
+	sq    __KVX_CONTEXT_R58R59[\to]= $r58r59      ## $r58 - $r59
 	;;
-	sq    __KVX_CONTEXT_R60R61[\to], $r60r61      ## $r60 - $r61
+	sq    __KVX_CONTEXT_R60R61[\to]= $r60r61      ## $r60 - $r61
 	;;
-	sq    __KVX_CONTEXT_R62R63[\to], $r62r63      ## $r62 - $r63
+	sq    __KVX_CONTEXT_R62R63[\to]= $r62r63      ## $r62 - $r63
 	;;
-	sq    __KVX_CONTEXT_SPC_LS[\to], $r2r3    ## $spc - $ls
+	sq    __KVX_CONTEXT_SPC_LS[\to]= $r2r3    ## $spc - $ls
 	;;
-	sq    __KVX_CONTEXT_LE_RA[\to], $r4r5     ## $le  - $ra
+	sq    __KVX_CONTEXT_LE_RA[\to]= $r4r5     ## $le  - $ra
 	;;
-	sq    __KVX_CONTEXT_SPS_CS[\to], $r6r7    ## $sps - $cs
+	sq    __KVX_CONTEXT_SPS_CS[\to]= $r6r7    ## $sps - $cs
 	;;
-	sd    __KVX_CONTEXT_LC[\to], $r8          ## $lc
+	sd    __KVX_CONTEXT_LC[\to]= $r8          ## $lc
 	;;
 .endm
 
@@ -182,7 +182,7 @@
 	andd  $r35 = $r35, $r33                    # drop the bits that are already identical to avoid useless set/clear attempts that may lead to PRIVILEGE hwtraps
 	lq.u  $r60r61  = __KVX_CONTEXT_R60R61[\from]
 	;;
-	wfxl  $sps = $r35                # finally restore $sps
+	wfxl  $sps , $r35                # finally restore $sps
 	;;
 	lq.u  $r58r59  = __KVX_CONTEXT_R58R59[\from]
 	;;

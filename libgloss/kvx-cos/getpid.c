825107fccfec5f7230776143ded1b4dd11b38c0f
Julien Villette <jvillette@kalray.eu>
2023-07-20 16:15:57 +0200

    Implement pseudo getpid for ClusterOS
    Summary: This pid is simply: 0x1000 | cluster id << 4 | cpu id
    
    Ref T17753
    
    Test Plan: CI
    
    Reviewers: O26 newlib!
    
    Differential Revision: https://phab.kalray.eu/D30681

--- libgloss/kvx-cos/getpid.c
+++ libgloss/kvx-cos/getpid.c
@@ -4,11 +4,12 @@
 
 #include <errno.h>
 
+#include <hal/cos_bsp.h>
+
 int
 _getpid ()
 {
-  errno = ENOSYS;
-  return -1;
+  return 0x1000 | (__cos_get_cluster_id() << 4) | __cos_get_cpu_id();
 }
 
 int getpid ()

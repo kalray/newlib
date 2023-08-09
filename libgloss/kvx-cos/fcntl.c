4a63b2e75372148e286d554c6ae9c93322e07fee
Julien Villette <jvillette@kalray.eu>
2023-08-09 11:43:32 +0200

    First level of fcntl syscall support for F_SETFL, F_GETFL, F_GETLK, F_SETLK and F_SETLKW commands
    Summary: Still have to implement remote version
    
    Test Plan: CI
    
    Reviewers: O26 newlib!
    
    Differential Revision: https://phab.kalray.eu/D31145

--- libgloss/kvx-cos/fcntl.c
+++ libgloss/kvx-cos/fcntl.c
@@ -16,7 +16,8 @@ int _fcntl(int fd, int command, long arg)
   int ret;
   int oflags = arg;
 
-  if (command == F_SETFL) {
+  if (command == F_SETFL || command == F_GETFL) {
+    if(command == F_SETFL) {
       oflags = 0;
       if (arg & O_RDONLY)   oflags |= S_RDONLY;
       if (arg & O_WRONLY)   oflags |= S_WRONLY;
@@ -31,14 +32,14 @@ int _fcntl(int fd, int command, long arg)
 #endif
       if (arg & O_NONBLOCK) oflags |= S_NONBLOCK;
       if (arg & O_NOCTTY)   oflags |= S_NOCTTY;
-  }
+    }
 
-  ret = sc_fcntl(fd,command,oflags);
+    ret = sc_fcntl(fd,command,oflags);
 
-  if( ret < 0 ) {
-    errno = ret * -1;
-    ret = -1;
-  } else if (command == F_GETFL) {
+    if( ret < 0 ) {
+      errno = ret * -1;
+      ret = -1;
+    } else if (command == F_GETFL) {
       arg = ret;
       ret = 0;
 
@@ -55,6 +56,50 @@ int _fcntl(int fd, int command, long arg)
 #endif
       if (arg & S_NONBLOCK) ret |= O_NONBLOCK;
       if (arg & S_NOCTTY)   ret |= O_NOCTTY;
+    }
+  }
+  else if( command == F_SETLK ||
+	   command == F_SETLKW ||
+	   command == F_GETLK ) {
+
+    /* struct flock {
+	 short	l_type;	        F_RDLCK, F_WRLCK, or F_UNLCK
+	 short	l_whence;	flag to choose starting offset
+	 long	l_start;	relative offset, in bytes
+	 long	l_len;		length, in bytes; 0 means lock to EOF
+	 short	l_pid;		returned with F_GETLK
+	 short	l_xxx;		reserved for future use
+       };
+    */
+
+    struct flock *fl = (struct flock*)(void*)arg;
+    uint64_t res[6];
+    res[0] = (uint64_t) fl->l_type;
+    res[1] = (uint64_t) fl->l_whence;
+    res[2] = (uint64_t) fl->l_start;
+    res[3] = (uint64_t) fl->l_len;
+    res[4] = (uint64_t) fl->l_pid;
+    res[5] = (uint64_t) fl->l_xxx;
+
+    ret = sc_fcntl(fd,command,(long)(uintptr_t)res);
+
+    if( ret < 0 ) {
+      errno = ret * -1;
+      ret = -1;
+    }
+    else {
+      fl->l_type = (short)res[0];
+      fl->l_whence = (short)res[1];
+      fl->l_start = (long)res[2];
+      fl->l_len = (long)res[3];
+      fl->l_pid = (short)res[4];
+      fl->l_xxx = (short)res[5];
+    }
+  }
+  else {
+    /* Command not supported yet */
+    errno = ENOSYS;
+    ret = -1;
   }
 
   return ret;

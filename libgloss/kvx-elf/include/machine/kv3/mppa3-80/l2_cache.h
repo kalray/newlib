0568caaa567442f6ce756fcc768ea4060262f7ff
Xavier Raynaud <xraynaud@kalrayinc.com>
2024-07-17 15:14:26 +0200

    add machine-generated kvx-elf files

---
+++ libgloss/kvx-elf/include/machine/kv3/mppa3-80/l2_cache.h
@@ -0,0 +1,17 @@
+/*
+ * l2_cache.h
+ * Automatically generated from registers definition files. Do not edit
+ * Copyright (c) Kalray 2024.
+ */
+
+#ifndef _INCLUDED_MPPA3_80_L2_CACHE_H_
+#define _INCLUDED_MPPA3_80_L2_CACHE_H_
+
+#ifdef __kvxarch_kv3_1
+#include "machine/kv3/mppa3-80/v1/l2_cache.h"
+#endif				/* __kvxarch_kv3_1 */
+
+#endif				// _INCLUDED_MPPA3_80_L2_CACHE_H_
+
+/* -*- Mode: C; c-basic-offset: 2 -*- */
+/* vim:set ft=c sw=2 cindent: */
